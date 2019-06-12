7392b733c86d9a2999820d97ccf324b0ee70bed1
zvash <Zvashkevich1999>
2019-06-12 20:00:00 +0300

    Добавил скрипты создание бд, но не тестил

--- src/main/java/bank/controllers/UserAccountController.java
+++ src/main/java/bank/controllers/UserAccountController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import sun.misc.Request;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
